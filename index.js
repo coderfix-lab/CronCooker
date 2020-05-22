2a6edbf094361a02ac881087e5196ebc87fdb08f
xiaoyu <316708138@qq.com>
2020-05-22 10:46:44 +0800

    add | 简单确定UI交互

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const {app, BrowserWindow} = require('electron')
 function createWindow () {
     // 创建浏览器窗口
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 350})
     // 然后加载应用的 index.html
     win.loadFile('html/index.html')
 }
